183baf79ead6d2b6ccda84f5da48dd11454a3308
Delici0u-s <maxi.bravo111@gmail.com>
2025-12-14 23:25:51 +0100

    :sob:

--- lua/configs/general/commands.lua
+++ lua/configs/general/commands.lua
@@ -1,5 +1,5 @@
 -- Create a :VT command for vertical terminal
-vim.api.nvim_create_user_command("vt", function()
+vim.api.nvim_create_user_command("VT", function()
 	vim.cmd("vsplit") -- vertical split
 	vim.cmd("terminal") -- open terminal in the new split
 end, {})
