577e77a0d0c66a9c954fdcd5fcd26ac13fa166f2
Delici0u-s <maxi.bravo111@gmail.com>
2025-12-15 13:59:59 +0100

    removed bla.py

--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
 -- print logfile location
 local log = require("utils.log")
 print("Logfile path: " .. log._log_path)
-log.write('New Session!!!')
+log.write("New Session!!!")
 
 -- loads custom configs, loads like everything lmao
 require("configs")
-
 
 -- print successfull loading of "packages" or whatever the fuck dis called
 log.write(vim.inspect(package.loaded["configs.plugins.cf_lspconfig"]))
@@ -14,5 +13,3 @@ log.write(vim.inspect(package.loaded["configs.general.keybinds"]))
 vim.o.shell = "zsh"
 vim.o.shellcmdflag = "-ic"
 vim.o.shellxquote = ""
-
-
