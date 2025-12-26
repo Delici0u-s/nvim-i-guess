cd2893d1efed66316ef0c056a29ab6fa6c8a5fdc
Delici0u-s <maxi.bravo111@gmail.com>
2025-12-26 20:08:15 +0100

    tmp

--- lua/configs/general/keybinds.lua
+++ lua/configs/general/keybinds.lua
@@ -3,21 +3,21 @@ local kb = require("utils.keybinds")
 
 -- remove highlighting
 kb.map(
-	"n",
-	"<C-l>",
-	[[ (&hls && v:hlsearch ? ':nohls' : ':set hls')."\n" <BAR> redraw<CR>]],
-	{ silent = true, expr = true }
+    "n",
+    "<C-l>",
+    [[ (&hls && v:hlsearch ? ':nohls' : ':set hls')."\n" <BAR> redraw<CR>]],
+    { silent = true, expr = true }
 )
 
 -- save on ctrl s
 kb.map("n", "<C-S>", ":w<cr>", { silent = true })
 kb.map("i", "<C-S>", function()
-	-- Exit insert mode
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
-	-- Save the file
-	vim.cmd("w")
-	-- Re-enter insert mode
-	vim.api.nvim_feedkeys("i", "n", true)
+    -- Exit insert mode
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+    -- Save the file
+    vim.cmd("w")
+    -- Re-enter insert mode
+    vim.api.nvim_feedkeys("i", "n", true)
 end, { silent = true })
 
 -- exit terminal insert mode
@@ -25,33 +25,39 @@ kb.map("t", "<C-X>", "<C-\\><C-n>", { silent = true })
 
 -- open terminal in vertical split
 kb.map("n", "vt", function()
-	vim.cmd("vsplit")
-	vim.cmd("terminal")
+    vim.cmd("vsplit")
+    vim.cmd("terminal")
 end, { desc = "Vertical terminal" })
 
 -- open terminal in vertical split
 kb.map("n", "vt", function()
-	vim.cmd("vsplit")
-	vim.cmd("terminal")
+    vim.cmd("vsplit")
+    vim.cmd("terminal")
 end, { desc = "Vertical terminal" })
 
 -- open terminal in horizontal split
 kb.map("n", "ht", function()
-	vim.cmd("split") -- corrected command
-	vim.cmd("terminal")
+    vim.cmd("split") -- corrected command
+    vim.cmd("terminal")
 end, { desc = "Horizontal terminal" })
 
 -- todo implement hotkey hook
 -- temp:
 -- execute macro for key after this combo
 kb.map("n", "<C-a>", function()
-	-- Prompt for a single key without needing Enter
-	vim.api.nvim_echo({ { "Press macro register:", "Normal" } }, false, {})
-	local reg = vim.fn.getchar() -- gets the keycode
-	reg = vim.fn.nr2char(reg) -- convert keycode to character
+    -- Prompt for a single key without needing Enter
+    vim.api.nvim_echo({ { "Press macro register:", "Normal" } }, false, {})
+    local reg = vim.fn.getchar() -- gets the keycode
+    reg = vim.fn.nr2char(reg)    -- convert keycode to character
 
-	-- Run the macro if a key was pressed
-	if reg ~= "" then
-		vim.api.nvim_feedkeys("@" .. reg, "n", true)
-	end
+    -- Run the macro if a key was pressed
+    if reg ~= "" then
+        vim.api.nvim_feedkeys("@" .. reg, "n", true)
+    end
 end, { silent = true })
+
+-- hex command
+kb.map("n", "hex", ":Hex<CR>", { desc = "Run :Hex command", silent = true })
+
+-- vex command
+kb.map("n", "vex", ":Vex<CR>", { desc = "Run :Vex command", silent = true })
--- lua/configs/plugins/cf_mason.lua
+++ lua/configs/plugins/cf_mason.lua
@@ -1,33 +1,34 @@
 return function()
-	local lspconfig = require("lspconfig")
-	local lsp = require("configs.plugins.cf_lsp")
+    local lspconfig = require("lspconfig")
+    local lsp = require("configs.plugins.cf_lsp")
 
-	require("mason-lspconfig").setup({
-		automatic_enable = true,
-		ensure_installed = {
-			-- Core
-			"lua_ls",
-			"pyright",
-			"clangd",
-			"jdtls",
+    require("mason-lspconfig").setup({
+        automatic_enable = true,
+        automaitc_setup = true,
+        ensure_installed = {
+            -- Core
+            "lua_ls",
+            "pyright",
+            "clangd",
+            "jdtls",
 
-			-- General
-			"bashls",
-			"jsonls",
-			"yamlls",
-			"marksman",
-			"cmake",
-			"mesonlsp",
+            -- General
+            "bashls",
+            "jsonls",
+            "yamlls",
+            "marksman",
+            "cmake",
+            "mesonlsp",
 
-			-- Web
-			"ts_ls",
-			"html",
-			"cssls",
-			"eslint",
+            -- Web
+            "ts_ls",
+            "html",
+            "cssls",
+            "eslint",
 
-			-- DevOps
-			"dockerls",
-			"terraformls",
-		},
-	})
+            -- DevOps
+            "dockerls",
+            "terraformls",
+        },
+    })
 end
