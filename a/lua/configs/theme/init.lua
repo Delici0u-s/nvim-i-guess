ca0bb71ac6849d4c18e5a112702c2bd296ccd307
Delici0u-s <maxi.bravo111@gmail.com>
2026-03-17 15:43:54 +0100

    lmfao

--- a/lua/configs/theme/init.lua
+++
@@ -1,8 +0,0 @@
-local type = "rainglow/"
-local theme = "bold"
--- local theme = "pastel-contrast"
-
-theme = type .. theme .. ".vim"
-
--- Source the vim file directly
-vim.cmd("source " .. vim.fn.stdpath("config") .. "/lua/configs/theme/themes/" .. theme)
