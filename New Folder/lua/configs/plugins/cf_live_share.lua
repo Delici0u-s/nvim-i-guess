1fa725348534c35e7d3c8854f51a1d8a3c41fcc4
Delici0u-s <maxi.bravo111@gmail.com>
2026-06-17 10:58:16 +0200

    remove failure

--- New Folder/lua/configs/plugins/cf_live_share.lua
+++
@@ -1,5 +0,0 @@
-return function()
-    vim.g.instant_username = "test_name"
-
-    require("live-share").setup({})
-end
--- New Folder/lua/configs/plugins/cf_render_markdown.lua
+++
@@ -1,9 +0,0 @@
-return function()
-	local kb = require("utils.keybinds")
-	local rm = require("render-markdown")
-
-	local opts = { file_types = { "markdown", "python" } }
-	rm.setup(opts)
-
-	kb.map("n", "<C-R>m", rm.toggle, { silent = true })
-end
