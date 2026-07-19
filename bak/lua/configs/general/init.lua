17bfed57ce397f260eb193b9dad2853c41c2ef35
Delici0u-s <maxi.bravo111@gmail.com>
2026-07-19 16:15:02 +0200

    forgot to delete bakup

--- bak/lua/configs/general/init.lua
+++
@@ -1,5 +0,0 @@
-local al = require('utils.auto_load')
-
-al.load_files_in_dir()
-al.load_folders_in_dir()
-
--- bak/lua/configs/init.lua
+++
@@ -1,7 +0,0 @@
-local al = require("utils.auto_load")
-
--- load theme before everything else
-require("configs.theme")
-
-al.load_files_in_dir()
-al.load_folders_in_dir()
